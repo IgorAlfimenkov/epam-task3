3f6e3fe5b1e6f45d870d83dc4ea98c9e96a7ff7a
IgorAlfimenkov <igoralfimenkov@gmail.com>
2021-11-12 12:49:24 +0300

    fix dispatcher troubles

--- src/main/java/com/alfimenkov/task3/ContainerDispatcher.java
+++ src/main/java/com/alfimenkov/task3/ContainerDispatcher.java
@@ -6,9 +6,20 @@ public class ContainerDispatcher extends Thread {
 
     private Port port;
     private ReentrantLock lock = new ReentrantLock();
+    private static ContainerDispatcher instance;
+    private boolean isStarted = false;
 
     public ContainerDispatcher(Port port) {
         this.port = port;
+    }
+
+    public static ContainerDispatcher getInstance(Port port) {
+
+        if(instance == null){
+
+            instance = new ContainerDispatcher(port);
+        }
+        return instance;
     }
 
     @Override
@@ -17,20 +28,33 @@ public class ContainerDispatcher extends Thread {
         try {
 
             sleep(100);
-            while (true){
+            while (!port.allDockFree()){
                 //sleep(100);
                 if(port.getStorageSize() == port.getMaxCapacity()) System.out.printf("Недастаточно места на складе. Невозможно добавить контейнеры\n");
-                while (port.getStorageSize() == port.getMaxCapacity()) sleep(10);
-                lock.lock();
-                Container container = port.takeContainerFromQueue();
-                port.addContainerInStorage(container);
-                lock.unlock();
+                while (port.getStorageSize() == port.getMaxCapacity() && !port.allDockFree()) sleep(10);
+
+                if(!port.queueIsEmpty()){
+                    lock.lock();
+                    Container container = port.takeContainerFromQueue();
+                    port.addContainerInStorage(container);
+                    lock.unlock();
+                }
+
             }
 
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+        finally {
+            System.out.printf("Диспетчер закончил работу\n");
+        }
     }
 
+    public boolean isStarted() {
+        return isStarted;
+    }
 
+    public void setStarted() {
+        isStarted = true;
+    }
 }
